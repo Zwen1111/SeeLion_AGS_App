55246bfb7ec2df97250a790b6fee653781ae16d2
Zwen 1111 <zwen12@msn.com>
2017-12-11 15:44:21 +0100

    Hein's Branch

--- app/src/main/java/com/application/ags/nl/seelion/Hardware/Vibration.java
+++ app/src/main/java/com/application/ags/nl/seelion/Hardware/Vibration.java
@@ -1,8 +1,20 @@
 package com.application.ags.nl.seelion.Hardware;
-
+import android.app.Activity;
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 /**
  * Created by zwen1 on 12/10/2017.
  */
 
 public class Vibration {
+
+    Vibrator v;
+
+    public Vibration(Activity activity){
+        v = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
+
+        // may crash, deprecated method, newer method only from api 26, this is min api 15
+        if(v != null){ v.vibrate(200);}
+    }
 }
--- app/src/main/java/com/application/ags/nl/seelion/UI/popups/Error.java
+++ app/src/main/java/com/application/ags/nl/seelion/UI/popups/Error.java
@@ -1,42 +1,8 @@
 package com.application.ags.nl.seelion.UI.popups;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+/**
+ * Created by zwen1 on 12/10/2017.
+ */
 
-import com.application.ags.nl.seelion.R;
-
-public class Error extends AppCompatActivity {
-
-    private Context econtext;
-
-    public Error(Context context){
-        this.econtext = context;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_error);
-    }
-
-    public void showError(){
-        final Dialog error = new Dialog(econtext);
-        error.setTitle("Error");
-
-        error.setContentView(R.layout.activity_error);
-
-        Button ok = (Button) error.findViewById(R.id.error_OkBtn);
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                error.cancel();
-            }
-        });
-
-        error.show();
-    }
+public class Error {
 }
