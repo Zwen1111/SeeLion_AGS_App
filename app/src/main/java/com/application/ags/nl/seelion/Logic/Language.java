109cbaac8629948a5e5fb71f7e50993878f5708b
hein van rooij <heinvanrooij@hotmail.com>
2017-12-18 14:15:01 +0100

    language change method needs testing
    needs testing

--- app/src/main/java/com/application/ags/nl/seelion/Logic/Language.java
+++ app/src/main/java/com/application/ags/nl/seelion/Logic/Language.java
@@ -1,8 +1,32 @@
 package com.application.ags.nl.seelion.Logic;
 
+import android.content.Context;
+import android.content.res.Configuration;
+
+import java.util.Locale;
+
 /**
  * Created by zwen1 on 12/10/2017.
  */
 
 public class Language {
+
+
+    private Context context;
+
+    public Language(Context context){
+        this.context = context;
+    }
+
+    // NEEDS TESTING
+    public void changeLanguage(String languageToChangeInto){
+        Locale locale = new Locale(languageToChangeInto);
+        Locale.setDefault(locale);
+
+        Configuration config = new Configuration();
+        config.locale = locale;
+
+        context.getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+    }
+
 }
